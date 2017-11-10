290ce9044cc55d78d45f4091136b7910f061f4ad
Akhil Narang <akhilnarang.1999@gmail.com>
2017-11-10 17:55:36 +0530

    kenzo: AOSiP bringup
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- aosip.mk
+++ aosip.mk
@@ -17,14 +17,14 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := lineage_kenzo
+PRODUCT_NAME := aosip_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
