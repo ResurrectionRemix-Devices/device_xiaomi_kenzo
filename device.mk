dc5086c6db80932a43cd8f4f6a8dbbda770a3dfc
Akhil Narang <akhilnarang.1999@gmail.com>
2018-01-12 21:28:57 +0530

    kenzo: button backlight and hwkeys stuff
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay $(LOCAL_PATH)/overlay-aosip
 
 # call the proprietary setup
 $(call inherit-product-if-exists, vendor/xiaomi/kenzo/kenzo-vendor.mk)
